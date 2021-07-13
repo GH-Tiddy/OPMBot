098de68f1c9e9f7949e84d43d3311cc5f1dff6cf
GH-Tiddy <47250863+GH-Tiddy@users.noreply.github.com>
2021-07-13 16:25:17 +0100

    Change search timings

--- testBot.py
+++ testBot.py
@@ -18,10 +18,10 @@ url = ""
 for submission in subreddit.new(limit = 1000):
     t = (time.time() - submission.created_utc) / 3600
     print(submission.link_flair_text, ": ", t)
-    if t > 96:
+    if t > 5:
         break
     if (submission.link_flair_text.lower() == "murata chapter" or submission.link_flair_text.lower() == "one chapter"):
-        if t < 96:
+        if t < 1:
           url = submission.url
           print(url)
           break
